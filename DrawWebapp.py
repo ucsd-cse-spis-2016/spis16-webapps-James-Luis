50b740b29aa07c3021b6c1d586e6c2c056a099ad
Luis C <lcalona@ucsd.edu>
2016-08-29 17:40:15 -0700

    added login feature for github

--- DrawWebapp.py
+++ DrawWebapp.py
@@ -1,8 +1,117 @@
+from flask import Flask, redirect, url_for, session, request, jsonify
+from flask_oauthlib.client import OAuth
+from flask import render_template, flash, Markup
+
+from github import Github
+
+import pprint
 import os
-from flask import Flask, session, url_for, render_template, request
+import sys
+import traceback
+
+class GithubOAuthVarsNotDefined(Exception):
+    '''raise this if the necessary env variables are not defined '''
+
+if os.getenv('GITHUB_CLIENT_ID') == None or \
+        os.getenv('GITHUB_CLIENT_SECRET') == None or \
+        os.getenv('APP_SECRET_KEY') == None or \
+        os.getenv('GITHUB_ORG') == None:
+    raise GithubOAuthVarsNotDefined('''
+      Please define environment variables:
+         GITHUB_CLIENT_ID
+         GITHUB_CLIENT_SECRET
+         GITHUB_ORG
+         APP_SECRET_KEY
+      ''')
 
 app = Flask(__name__)
-app.secret_key = 'eoNio48tL934Nudbn98hfY'
+app.secret_key = os.environ['APP_SECRET_KEY']
+oauth = OAuth(app)
+
+# This code originally from https://github.com/lepture/flask-oauthlib/blob/master/example/github.py
+# Edited by P. Conrad for SPIS 2016 to add getting Client Id and Secret from
+# environment variables, so that this will work on Heroku.
+
+github = oauth.remote_app(
+    'github',
+    consumer_key=os.environ['GITHUB_CLIENT_ID'],
+    consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],
+    request_token_params={'scope': 'read:org'},
+    base_url='https://api.github.com/',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://github.com/login/oauth/access_token',
+    authorize_url='https://github.com/login/oauth/authorize'
+)
+
+
+@app.context_processor
+def inject_logged_in():
+    return dict(logged_in=('github_token' in session))
+
+@app.context_processor
+def inject_github_org():
+    return dict(github_org=os.getenv('GITHUB_ORG'))
+
+@app.route('/login')
+def login():
+    return github.authorize(callback=url_for('authorized', _external=True))
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You were logged out')
+    return redirect(url_for('home'))
+
+@app.route('/login/authorized')
+def authorized():
+    resp = github.authorized_response()
+
+    if resp is None:
+        session.clear()
+        login_error_message = 'Access denied: reason=%s error=%s full=%s' % (
+            request.args['error'],
+            request.args['error_description'],
+            pprint.pformat(request.args)
+        )        
+        flash(login_error_message, 'error')
+        return redirect(url_for('home'))    
+
+    try:
+        session['github_token'] = (resp['access_token'], '')
+        session['user_data']=github.get('user').data
+        github_userid = session['user_data']['login']
+        org_name = os.getenv('GITHUB_ORG')
+    except Exception as e:
+        session.clear()
+        message = 'Unable to login: ' + str(type(e)) + str(e)
+        flash(message,'error')
+        return redirect(url_for('home'))
+    
+    try:
+        g = Github(resp['access_token'])
+        org = g.get_organization(org_name)
+        named_user = g.get_user(github_userid)
+        isMember = org.has_in_members(named_user)
+    except Exception as e:
+        message = 'Unable to connect to Github with accessToken: ' + resp['access_token'] + " exception info: " + str(type(e)) + str(e)
+        session.clear()
+        flash(message,'error')
+        return redirect(url_for('home'))
+    
+    if not isMember:
+        session.clear() # Must clear session before adding flash message
+        message = 'Unable to login: ' + github_userid + ' is not a member of ' + org_name + \
+          '</p><p><a href="https://github.com/logout" target="_blank">Logout of github as user:  ' + github_userid + \
+          '</a></p>' 
+        flash(Markup(message),'error')
+
+    else:
+        flash('You were successfully logged in')
+
+    return redirect(url_for('home'))  
+
+
 
 @app.route("/")
 def render_main():
@@ -15,6 +124,12 @@ def render_drawpage():
 @app.route("/testpage")
 def render_testpage():
     return render_template('testPage.html')
+
+
+@github.tokengetter
+def get_github_oauth_token():
+    return session.get('github_token')
+
 
 if __name__ == "__main__":
     app.run(debug=False, port=5001)
