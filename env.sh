b762df0368a9248841cc9c4155154222bfdaff03
Luis C <lcalona@ucsd.edu>
2016-08-31 08:51:56 -0700

    Added more tools and colors option

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
-export GITHUB_CLIENT_ID=  
-export GITHUB_CLIENT_SECRET=
+export GITHUB_CLIENT_ID=ed945e6cb1da32309ce6 
+export GITHUB_CLIENT_SECRET=090dbceb5cfab7c632a47a35bfffa040b67b02b9
 export APP_SECRET_KEY=f042eu9840qgah94YfhjeoagiLfa4e92387y271
 export GITHUB_ORG=ucsd-cse-spis-2016
 
